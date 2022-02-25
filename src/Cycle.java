2f8b6efe4863b2ae3172f50cd54c85f3e60e5102
zhenyka9696 <zhenyka9696@mail.ru>
2022-02-25 19:33:57 +0300

    Initial commit

---
+++ src/Cycle.java
@@ -0,0 +1,27 @@
+public class Cycle {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=9; i++)
+            System.out.println(i);
+        for(int i=1; i>1; i++)
+            System.out.print("hi");
+        for (int i=1;i<=9;i++)
+
+            System.out.print(i);
+            System.out.println();
+            for (int i=1;i<9;i++)
+            {
+                System.out.print(i);
+            }
+        System.out.println();
+            for (int i=9;i >=0;i--)
+                System.out.println(i);
+            
+            for(int i=1;i<12;i++)
+            {
+                System.out.println(i);
+                i+=a;
+                a++
+            }
+    }
+}
